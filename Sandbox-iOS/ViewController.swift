08e0cfe2471ff0ff7f13842276b18e6fc5a23607
beakhand <cat51_2@hotmail.co.jp>
2019-12-07 23:36:22 +0900

    first commit

---
+++ Sandbox-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sandbox-iOS
+//
+//  Created by beakhand on 2019/12/07.
+//  Copyright © 2019 beakhand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
